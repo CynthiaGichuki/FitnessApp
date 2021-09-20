5b6dc204c9bbbdd9f210701509e04978120b136a
CynthiaGichuki <gichukicynthia1@gmail.com>
2021-09-20 16:27:47 +0300

    GenderActivity and TabLayout

---
+++ app/src/main/java/com/example/project/GenderActivity.java
@@ -0,0 +1,51 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class GenderActivity extends AppCompatActivity {
+    private RadioButton male;
+    private RadioButton female;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender);
+
+        male = findViewById(R.id.selectMale);
+        female = findViewById(R.id.selectFemale);
+
+
+    }
+
+
+
+    public void gender(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.selectMale:
+                if (checked)
+                    Toast.makeText(GenderActivity.this,"Male",Toast.LENGTH_SHORT).show();
+                    break;
+            case R.id.selectFemale:
+                if (checked)
+                    Toast.makeText(GenderActivity.this,"Female",Toast.LENGTH_SHORT).show();
+                    break;
+        }
+    }
+
+    public void btnNext(View view) {
+        Intent intent = new Intent(GenderActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        //setSupportActionBar(toolbar);
+
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_3));
+        //set the tab to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        //use the pager adapter to manage screens
+        //create an instance of the view pager
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        //create a instance of the pagerAdapter
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        //set the adapter to the view pager
+        viewPager.setAdapter(pagerAdapter);
+        //set listener for clicks
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
