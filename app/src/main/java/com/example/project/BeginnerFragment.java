f62e14385c2b09f73ae7a9be77830dfa5884d6c7
CynthiaGichuki <gichukicynthia1@gmail.com>
2021-09-21 16:38:49 +0300

    Beginner Fragment

--- app/src/main/java/com/example/project/BeginnerFragment.java
+++ app/src/main/java/com/example/project/BeginnerFragment.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,22 @@ public class BeginnerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beginner, container, false);
-    }
+//        return inflater.inflate(R.layout.fragment_beginner, container, false);
+
+
+    View view = inflater.inflate(R.layout.fragment_beginner,
+            container, false);
+    ImageButton button = (ImageButton) view.findViewById(R.id.day1_img);
+   button.setOnClickListener(new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+           Intent intent = new Intent(getActivity(), ExerciseActivity.class);
+           startActivity(intent);
+        }
+    });
+   return view;
+}
+
 }
---
+++ app/src/main/java/com/example/project/ExerciseActivity.java
@@ -0,0 +1,61 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video5;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.seekTo(1);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        VideoView videoView2 = findViewById(R.id.video_view2);
+        String videoPath2 = "android.resource://" + getPackageName() + "/" + R.raw.video2;
+        Uri uri2 = Uri.parse(videoPath2);
+        videoView2.setVideoURI(uri2);
+        videoView2.seekTo(1);
+
+        MediaController mediaController2 = new MediaController(this);
+        videoView2.setMediaController(mediaController2);
+        mediaController2.setAnchorView(videoView2);
+
+
+
+        VideoView videoView3 = findViewById(R.id.video_view3);
+        String videoPath3 = "android.resource://" + getPackageName() + "/" + R.raw.video3;
+        Uri uri3 = Uri.parse(videoPath3);
+        videoView3.setVideoURI(uri3);
+        videoView3.seekTo(1);
+
+        MediaController mediaController3 = new MediaController(this);
+        videoView3.setMediaController(mediaController3);
+        mediaController3.setAnchorView(videoView3);
+
+
+        VideoView videoView4 = findViewById(R.id.video_view4);
+        String videoPath4 = "android.resource://" + getPackageName() + "/" + R.raw.plank;
+        Uri uri4 = Uri.parse(videoPath4);
+        videoView4.setVideoURI(uri4);
+        videoView4.seekTo(1);
+
+        MediaController mediaController4 = new MediaController(this);
+        videoView4.setMediaController(mediaController4);
+        mediaController4.setAnchorView(videoView4);
+
+    }
+}
